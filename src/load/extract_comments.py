467ae032280a78df18f7ad8bb5eab03d1f7cd20f
Roy Smith <roysmith@roys-mac-mini.mynetworksettings.com>
2023-12-24 13:40:23 -0500

    Add --tuple option to reduce run time by about 3x.

--- src/load/extract_comments.py
+++ src/load/extract_comments.py
@@ -8,23 +8,55 @@ Extract edit comments.
 
 import argparse
 import bz2
+from collections import namedtuple
 from pathlib import Path
 
 import schema
 
+EscapedRow = namedtuple("EscapedRow", schema.field_names)
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('filename')
+    parser.add_argument('filename',
+                        help='input file (must be in tsv-bz2 format)')
+    parser.add_argument('--tuple',
+                        default=False,
+                        action=argparse.BooleanOptionalAction,
+                        help='Use the high-speed NamedTuple parsing strategy')
     args = parser.parse_args()
-    for row in get_rows(args.filename):
+
+    if args.tuple:
+        process_as_tuples(args.filename)
+    else:
+        process_as_rows(args.filename)
+
+
+def process_as_tuples(filename):
+    for row in get_tuples(filename):
+        if row.event_entity == 'revision':
+            comment = schema.unescape_tnr(row.event_comment_escaped)
+            if comment and not row.event_user_is_bot_by_string:
+                summary = get_human_text(comment)
+                if summary:
+                    print(schema.escape_tnr(summary))
+
+
+def get_tuples(filename):
+    with bz2.open(filename, 'rt') as f:
+        for line in f:
+            yield EscapedRow(*line.split('\t'))
+
+
+def process_as_rows(filename):
+    for row in get_rows(filename):
             if row.event_entity == 'revision' and row.event_comment and not row.event_user_is_bot_by:
                 summary = get_human_text(row.event_comment)
                 if summary:
-                    print(summary)
+                    print(schema.escape_tnr(summary))
 
 
-def get_rows(file_name):
-    with bz2.open(file_name, 'rt') as f:
+def get_rows(filename):
+    with bz2.open(filename, 'rt') as f:
         for line in f:
             yield schema.build_row(line)
 
